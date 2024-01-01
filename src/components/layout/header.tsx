e8f114812e9549e30772e990ef4005483af21dfc
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2024-01-01 20:59:13 +0800

    style: initial empty components

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Header(){
-    return <header className={"container flex justify-between"}>
+    return <header className={"container flex justify-between py-8"}>
         <div>
             Logo Here
         </div>
