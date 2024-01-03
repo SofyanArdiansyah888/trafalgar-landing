9ddd7b89ebdbdaa7e33e7b85186f3c23e01b2bd8
Sofyan Ardiansyah <sofyanardianysah@gmail.com>
2024-01-03 13:53:46 +0800

    style: change hover color

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LogoIcon from "@/components/ui/icons/logo-icon";
 import Typography from "@/components/ui/typography";
+import {Link} from "gatsby";
 
 export default function Header() {
     return <header className={"container flex justify-between py-8 items-center"}>
@@ -11,26 +12,26 @@ export default function Header() {
         <nav>
             <ul className={"flex gap-8 items-center"}>
                 <li>
-                    <Typography thick={"semibold"} size={'textSmall'}> Home</Typography>
+                    <Typography as={"a"} href={"#home"}  color={"muted"} size={'textSmall'} className={"hover:text-primary"} > Home</Typography>
                 </li>
                 <li>
-                    <Typography color={"muted"} size={'textSmall'}>
+                    <Typography as={"a"} href={"#service"}  color={"muted"} size={'textSmall'} className={"hover:text-primary"}>
                         Find A Doctor
                     </Typography>
                 </li>
                 <li>
-                    <Typography color={"muted"} size={'textSmall'}>
+                    <Typography as={"a"} href={"#download"} color={"muted"} size={'textSmall'} className={"hover:text-primary"}>
                         Apps
                     </Typography>
                 </li>
                 <li>
-                    <Typography color={"muted"} size={'textSmall'}>
+                    <Typography as={"a"} href={"#client"} color={"muted"} size={'textSmall'} className={"hover:text-primary"}>
                         Testimonials
                     </Typography>
                 </li>
                 <li>
-                    <Typography color={"muted"} size={'textSmall'}>
-                        About Us
+                    <Typography as={"a"} href={"#article"} color={"muted"} size={'textSmall'} className={"hover:text-primary"}>
+                        Article
                     </Typography>
                 </li>
             </ul>
